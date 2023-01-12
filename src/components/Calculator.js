1e7ba47a3ab8d2c9128387d60d7e5fad1f95c9db
EddyShimwa <shedpray16@gmail.com>
2023-01-12 23:24:32 +0200

    added event handlers on every button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,88 +1,103 @@
 import React from 'react';
+import calculate from './logic/calculate';
+// import operate from './logic/operate';
 import './calculator.css';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
-  render() {
-    return (
-      <div className="calculator-container">
-        <div className="input-holder">
-          <input className="input" type="text" value="0" readOnly />
+    clickHandler = (e) => {
+      const result = calculate(this.state, e.target.textContent);
+      this.setState((prevState) => ({
+        ...prevState,
+        ...result,
+      }));
+    };
+
+    render() {
+      const { total, next } = this.state;
+      return (
+        <div className="calculator-container">
+          <div className="input-holder">
+            <div className="input" type="text">{ next || total || 0}</div>
+          </div>
+          <ul className="rows">
+            <li className="row">
+              <button className="operator-btn" type="button" onClick={this.clickHandler}>
+                AC
+              </button>
+              <button className="operator-btn" type="button" onClick={this.clickHandler}>
+                +/-
+              </button>
+              <button className="operator-btn" type="button" onClick={this.clickHandler}>
+                %
+              </button>
+              <button className="operator-btn-orange" type="button" onClick={this.clickHandler}>
+                &#247;
+              </button>
+            </li>
+            <li className="row">
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                7
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                8
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                9
+              </button>
+              <button className="operator-btn-orange" type="button" onClick={this.clickHandler}>
+                x
+              </button>
+            </li>
+            <li className="row">
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                4
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                5
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                6
+              </button>
+              <button className="operator-btn-orange" type="button" onClick={this.clickHandler}>
+                -
+              </button>
+            </li>
+            <li className="row">
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                1
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                2
+              </button>
+              <button className="digit-btn" type="button" onClick={this.clickHandler}>
+                3
+              </button>
+              <button className="operator-btn-orange" type="button" onClick={this.clickHandler}>
+                +
+              </button>
+            </li>
+            <li className="row">
+              <button className="digit-btn zero" type="button" onClick={this.clickHandler}>
+                0
+              </button>
+              <button className="operator-btn" type="button" onClick={this.clickHandler}>
+                .
+              </button>
+              <button className="operator-btn-orange" type="button" onClick={this.clickHandler}>
+                =
+              </button>
+            </li>
+          </ul>
         </div>
-        <ul className="rows">
-          <li className="row">
-            <button className="operator-btn" type="button">
-              AC
-            </button>
-            <button className="operator-btn" type="button">
-              +/-
-            </button>
-            <button className="operator-btn" type="button">
-              %
-            </button>
-            <button className="operator-btn-orange" type="button">
-              &#247;
-            </button>
-          </li>
-          <li className="row">
-            <button className="digit-btn" type="button">
-              7
-            </button>
-            <button className="digit-btn" type="button">
-              8
-            </button>
-            <button className="digit-btn" type="button">
-              9
-            </button>
-            <button className="operator-btn-orange" type="button">
-              x
-            </button>
-          </li>
-          <li className="row">
-            <button className="digit-btn" type="button">
-              4
-            </button>
-            <button className="digit-btn" type="button">
-              5
-            </button>
-            <button className="digit-btn" type="button">
-              6
-            </button>
-            <button className="operator-btn-orange" type="button">
-              -
-            </button>
-          </li>
-          <li className="row">
-            <button className="digit-btn" type="button">
-              1
-            </button>
-            <button className="digit-btn" type="button">
-              2
-            </button>
-            <button className="digit-btn" type="button">
-              3
-            </button>
-            <button className="operator-btn-orange" type="button">
-              +
-            </button>
-          </li>
-          <li className="row">
-            <button className="digit-btn zero" type="button">
-              0
-            </button>
-            <button className="operator-btn" type="button">
-              .
-            </button>
-            <button className="operator-btn-orange" type="button">
-              =
-            </button>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+      );
+    }
 }
--- src/components/logic/calculate.js
+++ src/components/logic/calculate.js
@@ -131,4 +131,3 @@ export default function calculate(obj, buttonName) {
     operation: buttonName,
   };
 }
-
