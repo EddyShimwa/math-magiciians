b6c1b207eecf8db11cb8f05247be65bad1913068
EddyShimwa <shedpray16@gmail.com>
2023-01-19 08:57:02 +0200

    added the quotes page

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const home = () => {
+   <div>
+    <h2>Welcome to Our Page</h2>
+    <p>
+    Welcome to our calculator app! Our app is designed to make your calculations quick and easy. Whether you're a student, a professional, or just someone who needs to make a calculation from time to time, our app is the perfect tool for you. With a user-friendly interface and a wide range of functions, you can perform any calculation you need in just a few clicks. From basic arithmetic to more advanced calculations, our app has got you covered. Download now and see for yourself how our calculator app can make your life easier.
+    </p>
+   </div>
+}
+
+export default home
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
