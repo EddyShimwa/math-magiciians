1ddcce3807db24f4825cf6ec2aed7e52f0d47264
EddyShimwa <shedpray16@gmail.com>
2023-01-19 11:47:33 +0200

    fixed the linter errors

--- src/Pages/calculatorPage.js
+++ src/Pages/calculatorPage.js
@@ -1,2 +1,25 @@
-import React from "react";
-import Calculator from "../components/Calculator";
+import React from 'react';
+
+import Calculator from '../components/Calculator';
+import Card from '../components/UI/Card';
+import classes from './CalculatorPage.module.css';
+
+const CalculatorPage = () => (
+  <section className={classes.calculator_part}>
+    <Card extraclass={classes.calc_details}>
+      <h3>Lets do some Maths!</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        numquam atque, ipsam sint molestiae doloremque consequatur nam dolorum
+        eum fuga ut similique earum saepe nisi odit placeat quos nihil aut.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad odit
+        repudiandae mollitia, illum, excepturi repellendus cum corrupti hic
+        voluptatibus odio enim sunt necessitatibus dolorum dolor! Iure quae
+        officia aperiam delectus.
+      </p>
+    </Card>
+    <Calculator />
+  </section>
+);
+
+export default CalculatorPage;
